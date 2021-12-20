d12b14f784115acf47ad08f2e55ce506f826fdcf
Ben Hill <ben.hill@kneat.com>
2021-12-20 19:30:53 +0000

    updated step definition with basic working test examples need to refactor custom commands

--- cypress/integration/DemoTests/Web_Disclosure/Web_Disclosure.js
+++ cypress/integration/DemoTests/Web_Disclosure/Web_Disclosure.js
@@ -1,5 +1,7 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+var countryCount = 0;
+var countryElementtdRowCount = 0;
 
 Given('user is on the landing page for WD site', () => {
     cy.visit(Cypress.env('WDURL'))
@@ -32,12 +34,19 @@ When('clicks on Update button for the country filter list', () => {
 
             // Get responce boby and count the number of accounts and get the country 
             const object = res.body
+            
 
-            console.log(object.Data)
+            //console.log(object.Data)
             for (let i = 0; i < object.Data.length; i++) {
-                console.log(object.Data[i].Country);
+                //console.log(object.Data[i].Country);
+                if (object.Data[i].Country == 'Belgium') {
+                    countryCount++;
+                    
+
+                }
 
             }
+            console.log('countryCount = ' + countryCount)
             
         })
     })
@@ -54,7 +63,7 @@ When('clicks on Update button for the country filter list', () => {
             // can access the low level interception that contains the request body,
             // response body, status, etc
 
-            cy.log(response)
+            //cy.log(response)
            
         })
 
@@ -67,18 +76,52 @@ When('clicks on Update button for the country filter list', () => {
 
 
 Then('the grid displays all meetings that are associated with the country {string}', (countryDisplayed) => {
-    //cy.pause()
-    cy.get('table > tbody > tr > td:nth-child(5)')
-    cy.contains('Belgium')
+
+    // The grid only displays meetings that are associated with the selected filter
  
-    cy.get('tbody').should('have.attr', 'role', 'rowgroup').find('tr').contains('td',"Belgium").then(($el) => {
-        cy.get($el)
+    cy.get('tbody').should('have.attr', 'role', 'rowgroup').find('tr').contains('td', countryDisplayed).then(($el) => {
+        cy.get($el).should('contain', countryDisplayed)
+       
     })
+    // the number of table rows with the selected filter is equal to the number returned in the object from the API call 
+    cy.get('table > tbody > tr > td:nth-child(6)').should('contain', countryDisplayed).each(($el, index, list) => {
+
+    }).then((list) => {
+        expect(list).to.have.length(countryCount)
+
+    })
+    
+    
+    //cy.log(countryCount)
 })
 
 
 
 Then('no meetings associated with any other country appear on the list', () => {
+
+    
+ 
+
+    cy.get('table > tbody > tr > td:nth-child(6)').then(($el, index) => {
+        
+      
+        //cy.get($el).invoke('text').should('contain', 'Belgium')
+        
+        for (let i = 0; i < $el.length; i++) {
+            //var testtext = cy.get($el[i]).invoke('text')
+            
+            
+            if (expect($el[i]).to.contain('Belgium')) {
+                countryElementtdRowCount++;
+
+            }
+            
+        }
+        // if the $el.length and the countryElementtdRowCount are the same then only Belgium is rendered in the page
+        expect($el.length).to.equal(countryElementtdRowCount)
+
+    })
+
 
 })
 
