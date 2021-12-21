91a27cc6484befc8b0db0c5f55ebea0052b5e0e3
Ben Hill <ben.hill@kneat.com>
2021-12-21 15:30:38 +0000

    Added comments to test case step definition files

--- cypress/integration/DemoTests/CompanyFilter/CompanyFilter.js
+++ cypress/integration/DemoTests/CompanyFilter/CompanyFilter.js
@@ -1,19 +1,28 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+// Given steps are in common folder with file named after test step  
 
-
-//Company filter test 
+//Company filter test - select company filter link from page 2 of the grid list  
 When('user clicks the Company Name {string} hyper-link', (companyLink) => {
 
     cy.get('[aria-label="Go to the next page"]').click()
+    // assert that page 
     cy.get('.k-current-page >.k-link').should('contain', '2')
+
+    // click on element that contains company link 
     cy.contains(companyLink).click()
 
 })
 
 // Company filter test
 Then('the user lands onto the {string} vote card page.', (companyDisplayed) => {
-    cy.log(Cypress.env('ATVI_securityId'))
+
+   // cy.log(Cypress.env('ATVI_securityId'))
+
+    // Assert that the company name is visible
+    cy.contains(companyDisplayed).should('be.visible')
+
+    // Assert that the security id in the URL is equal to that set up for the company
     cy.url().should('include', Cypress.env('ATVI_securityId'))
     cy.get('#txt-detail-info-Security-Id-From-Distributor').should('contain', '00507V109')
 
@@ -21,5 +30,7 @@ Then('the user lands onto the {string} vote card page.', (companyDisplayed) => {
 
 // Company filter test
 Then('{string} should appear in the top banner', (companyDisplayed) => {
+
+    //assert that the company name is in the banner element 
     cy.get('#detail-issuer-name').should('contain', companyDisplayed)
 })
--- cypress/integration/DemoTests/Web_Disclosure/Web_Disclosure.js
+++ cypress/integration/DemoTests/Web_Disclosure/Web_Disclosure.js
@@ -1,22 +1,25 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+// Declare and initialize global variable
 var countryCount = 0;
 var countryElementtdRowCount = 0;
 
+// Given steps are in common folder with file named after test step  
 
-
+// test that the country filter is visible
 Given('the Country filter is available', () => {
-    cy.get('table > tbody > tr > td:nth-child(6)')
-    cy.contains('Belgium')
+
     cy.get('#txt-multiselect-static-search-CountryFilter').should('have.attr', 'type', 'search').and('be.visible')
 })
 
+// Select the country filter based on the feature file variable
 When('user selects {string} from the Country filter list on left panel', (Country_Filter) => {
 
     cy.get('#' + Country_Filter + '-cb-label-CountryFilter').should('contain', Country_Filter)
     cy.get('#Belgium-cb-label-CountryFilter').click()
 })
 
+// Click the update button
 When('clicks on Update button for the country filter list', () => {
     //Wait for aliased route as 'getCountry' to respond with out changing or stubbing the response 
     // to avoid race condition on click update and to use data to verify only Belgium is displayed
@@ -24,7 +27,8 @@ When('clicks on Update button for the country filter list', () => {
     // Intercept API call to data and inspect response data
 
    
-   // Could refactor as a custom command  
+   // Could re-factor as a custom command  
+    // Spy on the API request and inspect the data object 
     cy.intercept('https://viewpoint.glasslewis.com/WD/Api/Data//Issuers', (req) => {
         req.on('response', (res) => {
     // this will be called after all `before:response` handlers and after the `req.continue` handler
@@ -54,6 +58,7 @@ When('clicks on Update button for the country filter list', () => {
     cy.get('#multiselect-static-target-CountryFilter > .btn-container > #btn-update').then(($btn) => {
         const cls = $btn.attr('class')
         cy.wrap($btn).should('contain', 'Update').click()
+
         // get at the response body from the the API call 
         // wait for API call to return body 
         cy.wait('@getCountry').then((response) => {
@@ -63,7 +68,7 @@ When('clicks on Update button for the country filter list', () => {
             //cy.log(response)
            
         })
-
+        // test button class and text value
         cy.wrap($btn).should('have.class', cls)
         cy.wrap($btn).should('contain', 'Update')
     })
@@ -71,7 +76,7 @@ When('clicks on Update button for the country filter list', () => {
 
 })
 
-
+// Compare the data object with the DOM elements 
 Then('the grid displays all meetings that are associated with the country {string}', (countryDisplayed) => {
 
     // The grid only displays meetings that are associated with the selected filter
